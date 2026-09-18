662045b5e1c2415143958cad34ed32b8db401bcc
tony <tony.hetrick@gmail.com>
2026-09-18 14:42:29 +0300

    Labeling tweaks

--- build-context/generate-salts.sh
+++ build-context/generate-salts.sh
@@ -3,22 +3,28 @@
 
 KEYS=("AUTH_KEY" "SECURE_AUTH_KEY" "LOGGED_IN_KEY" "NONCE_KEY" "AUTH_SALT" "SECURE_AUTH_SALT" "LOGGED_IN_SALT" "NONCE_SALT")
 
-# Generate and store the salts
+# Generate and store the salts for .env file
 declare -A SALTS
 for key in "${KEYS[@]}"; do
     SALTS[$key]=$(openssl rand -hex 20)
 done
 
 echo "================================================================="
-echo "1. Add these to your .env file (if passing via docker-compose):"
+echo "Add these to your .env file (if passing via docker-compose):"
 echo "================================================================="
 for key in "${KEYS[@]}"; do
     echo "WORDPRESS_${key}=${SALTS[$key]}"
 done
 
+# Generate new salts for wp-config.php
+declare -A SALTS
+for key in "${KEYS[@]}"; do
+    SALTS[$key]=$(openssl rand -hex 20)
+done
+
 echo ""
 echo "================================================================="
-echo "2. Add these to your wp-config.php file:"
+echo "Add these to your wp-config.php file:"
 echo "================================================================="
 for key in "${KEYS[@]}"; do
     # Pad the entire string segment instead of the variable name
